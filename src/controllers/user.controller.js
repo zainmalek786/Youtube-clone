9fa1e111403704e9864e453c99fbb820c3122382
Zain Akbar Malik <zainakbarmalik8@gmail.com>
2024-09-29 10:52:15 -0700

    code updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -160,4 +160,36 @@ const loginUser = asyncHandler( async (req,res)=>{
 
 })
 
-export {registerUser}
+const logOutUser = asyncHandler( async(req,res)=>{
+   User.findByIdAndUpdate(
+    req.user._id,
+    {
+        $set:
+        {
+             refreshToken: undefined
+        }
+    },
+    {
+        new:true
+    }
+   )
+
+   const options ={
+    httpOnly:true,
+    secure:true
+   }
+
+   return res
+   .status(200)
+   .clearCookie("accessToken",options)
+   .clearCookie("refreshToken",options)
+   .json(
+    new ApiResponse(200,{},"User Logged Out")
+   )
+
+})
+
+export {registerUser,
+    loginUser,
+    logOutUser
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logOutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router = Router()
 
@@ -16,5 +18,9 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+
+router.route("/login").post(verifyJWT,logOutUser)
 
 export default router
